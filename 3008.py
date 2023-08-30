7e363e8a45a08369323584d723a78842ffd5e1d0
luisllanca <63575034+luisllanca@users.noreply.github.com>
2023-08-30 12:09:19 -0400

    Create 3008.py

---
+++ 3008.py
@@ -0,0 +1,19 @@
+def info_a_tupla(arte):
+    pass
+    
+def encuentra_artistas_unicos(tupla):
+    pass
+
+def contar_canciones(artista, canciones):
+    pass
+
+def main():
+    arte = "easykid-tanjiro,drefquila-afuego,drefquila-demasiao,jordan23-ando"
+
+    
+    # arte_tupla = info_a_tupla()
+    # artistas_unicos = encuentra_artistas_unicos()
+    # encontrar cantidad de canciones por cada aritsta (usar un ciclo for)
+    
+
+main()
